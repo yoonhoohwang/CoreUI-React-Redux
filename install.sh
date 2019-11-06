ea8b9c7d7fbddef7f8e4b4ced81561375a3f8445
Jeff <yoonhu1431@gmail.com>
2019-11-06 15:13:59 +0900

    Create Repo

---
+++ install.sh
@@ -0,0 +1,16 @@
+npm install --save-dev \
+    prettier \
+    eslint-plugin-jest \
+    eslint-plugin-react \
+    eslint-plugin-import \
+    eslint-config-airbnb \
+    eslint-plugin-promise \
+    eslint-plugin-prettier \
+    eslint-plugin-react-hooks \
+    eslint-config-prettier
+
+npm i -D eslint babel-eslint
+
+# create react app already install
+# jest \
+# eslint \
